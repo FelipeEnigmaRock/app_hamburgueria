58312abe5e28f64bb4075a4f6a4cdad4c9f22a2a
FelipeEnigmaRock <felj.gomes@gmail.com>
2022-04-27 21:45:40 -0300

    Commit inicial
    Confecção de site de hamburgueria de curso sobre vue.js

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+      <router-link to="/">
+          <img src="" alt="" id="logo">
+      </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/Pedidos">Pedidos</router-link>
+  </nav>
+</template>
+<script>
+export default {
+    name: "Navbar",
+    props: ["logo", "alt"]
+}
+</script>
+<style scoped>
+  
+</style>
