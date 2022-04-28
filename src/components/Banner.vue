4bbd3e102e02690d104f2f6661b8f29b023bf1ac
FelipeEnigmaRock <felj.gomes@gmail.com>
2022-04-28 19:09:27 -0300

    Home incial
    Criação da primeira parte da primeira página

---
+++ src/components/Banner.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="main-banner">
+    <h1>Make Your Burguer!</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Banner",
+};
+</script>
+
+<style scoped>
+#main-banner {
+  background-image: url('/public/img/burger.jpg');
+  background-position: 0 -450px;
+  background-size: cover;
+  width: 100%;
+  height:30vw;
+  display:flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+#main-banner h1 {
+    color: #FFF;
+    text-align: center;
+    font-size: 3.5vw;
+    background: rgba(34, 34, 34, 0.473);
+    padding: 20px 40px;
+}
+</style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,25 @@
 <template>
-    <p>Footer</p>
+    <footer>
+        <p>Make Your Burger &copy; 2022</p>
+    </footer>
 </template>
 <script>
 export default {
     name: "Footer"
 }
 </script>
+<style scoped>
+    footer {
+        background: #222;
+        height: 200px;
+        border-top: 4px solid #111;
+        color: #FCBA03;
+        text-align: center;
+        width: 100%;
+        position: relative;
+        bottom: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
